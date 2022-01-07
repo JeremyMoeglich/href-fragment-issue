ff22aa581b5d80e69f2c09464ba7f2e65f710278
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-01-07 03:40:40 +0100

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h2>This file is not related to the issue</h2>
+<a href="/test/route">Click here to go to the right path</a>
---
+++ src/routes/test/route.svelte
@@ -0,0 +1,3 @@
+<p>This file should have no errors</p>
+
+<a href="#something" id='something'>This causes an Error when building using static adapter</a>
